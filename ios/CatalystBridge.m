43c3e1c9e967699a667c81adb8eea4fe356a2aa7
syntheticencounters <36394356+syntheticencounters@users.noreply.github.com>
2019-12-27 17:08:12 -0600

    Update CatalystBridge.m

--- ios/CatalystBridge.m
+++ ios/CatalystBridge.m
@@ -10,8 +10,8 @@ RCT_EXPORT_MODULE()
 }
 
 RCT_EXPORT_METHOD(setWindowSize:
-                  width:(nonnull NSNumber *)
-                  height:(nonnull NSNumber *)
+                  (nonnull NSNumber *)width
+                  height:(nonnull NSNumber *)height
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
 
@@ -20,6 +20,25 @@ RCT_EXPORT_METHOD(setWindowSize:
             UIWindowScene* windowScene = (UIWindowScene*) scene;
             windowScene.sizeRestrictions.minimumSize = CGSizeMake([width doubleValue], [height doubleValue]);
             windowScene.sizeRestrictions.maximumSize = CGSizeMake([width doubleValue], [height doubleValue]);
+
+            resolve(@"Window size set");
+            return;
+        }
+    }
+    reject(@"Unable to set window size", @"Unable to set window size", [NSError errorWithDomain:@"ecarra" code:400 userInfo:NULL]);
+}
+
+RCT_EXPORT_METHOD(setMinWindowSize:
+                  (nonnull NSNumber *)width
+                  height:(nonnull NSNumber *)height
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+
+    for (UIScene* scene in UIApplication.sharedApplication.connectedScenes) {
+        if ([scene isKindOfClass:[UIWindowScene class]]) {
+            UIWindowScene* windowScene = (UIWindowScene*) scene;
+            windowScene.sizeRestrictions.minimumSize = CGSizeMake([width doubleValue], [height doubleValue]);
+            windowScene.sizeRestrictions.maximumSize = CGSizeMake(10000, 10000);
 
             resolve(@"Window size set");
             return;
