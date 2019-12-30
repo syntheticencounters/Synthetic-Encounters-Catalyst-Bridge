bfeacb5c3c0d94647d3dea4421eedf1d024312f9
syntheticencounters <36394356+syntheticencounters@users.noreply.github.com>
2019-12-30 16:34:48 -0600

    Updates

--- ios/CatalystBridge.m
+++ ios/CatalystBridge.m
@@ -47,4 +47,22 @@ RCT_EXPORT_METHOD(setMinWindowSize:
     reject(@"Unable to set window size", @"Unable to set window size", [NSError errorWithDomain:@"ecarra" code:400 userInfo:NULL]);
 }
 
+RCT_REMAP_METHOD(matchDisplaySize,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+
+    for (UIScene* scene in UIApplication.sharedApplication.connectedScenes) {
+        if ([scene isKindOfClass:[UIWindowScene class]]) {
+            UIWindowScene* windowScene = (UIWindowScene*) scene;
+            windowScene.sizeRestrictions.minimumSize = UIScreen.mainScreen.currentMode.size;
+            windowScene.sizeRestrictions.maximumSize = CGSizeMake(10000, 10000);
+
+            resolve(@"Window size set");
+            return;
+        }
+    }
+    reject(@"Unable to set window size", @"Unable to set window size", [NSError errorWithDomain:@"ecarra" code:400 userInfo:NULL]);
+}
+
 @end
